eccb02fddec57d70b61514c0c8ac1e8428ca2418
muna <muna.alharbi.90@gmail.com>
2022-01-04 08:20:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mcallbacksapp1/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.mcallbacksapp1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navigateButton = findViewById<Button>(R.id.navigate_btn)
+//        navigateButton.setOnClickListener {
+//            val intent = Intent(this, SecondActivity::class.java)
+//            startActivity(intent)
+//        }
+        navigateButton.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?){
+                val intent = Intent(this@MainActivity, SecondActivity::class.java)
+                startActivity(intent)
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume Activity 1", Toast.LENGTH_SHORT).show()
+        Log.d("First Activity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause Activity 1", Toast.LENGTH_SHORT).show()
+        Log.d("First Activity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop Activity 1", Toast.LENGTH_SHORT).show()
+        Log.d("First Activity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy Activity 1", Toast.LENGTH_SHORT).show()
+        Log.d("First Activity", "onDestroy")
+    }
+}
---
+++ app/src/main/java/com/example/mcallbacksapp1/SecondActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mcallbacksapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume Activity 2", Toast.LENGTH_SHORT).show()
+        Log.d("Second Activity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause Activity 2", Toast.LENGTH_SHORT).show()
+        Log.d("Second Activity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop Activity 2", Toast.LENGTH_SHORT).show()
+        Log.d("Second Activity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy Activity 2", Toast.LENGTH_SHORT).show()
+        Log.d("Second Activity", "onDestroy")
+    }
+}
